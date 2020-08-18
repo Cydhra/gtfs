652c0436d2a5b5a30f6172cfe9137e5e71d932c2
Max Göttlicher <maxgoett@yahoo.de>
2020-08-18 20:12:42 +0200

    stop class

--- src/main/kotlin/net/tmbt/gtfs/model/Stop.kt
+++ src/main/kotlin/net/tmbt/gtfs/model/Stop.kt
@@ -1,5 +1,7 @@
 package net.tmbt.gtfs.model
 
+import org.jetbrains.exposed.dao.Entity
+import org.jetbrains.exposed.dao.EntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IdTable
 import org.jetbrains.exposed.sql.Column
@@ -32,9 +34,28 @@ object StopTable : IdTable<String>() {
     val stopTimezone = text("stop_timezone").nullable()
     val wheelchairBoarding = enumeration("wheelchair_boarding", WheelchairBoarding::class).nullable()
     //val levelId = reference("level_id", TODO level table).nullable()
-    val platform_code = text("platform_code").nullable()
+    val platformCode = text("platform_code").nullable()
 
     override val id: Column<EntityID<String>>
         get() = TODO("Not yet implemented")
+}
+
+class Stop(id: EntityID<String>) : Entity<String>(id) {
+    companion object : EntityClass<String, Agency>(AgencyTable)
+
+    val stopId by StopTable.stopId
+    val stopCode by StopTable.stopCode
+    val stopName by StopTable.stopName
+    val stopDesc by StopTable.stopDesc
+    val stopLat by StopTable.stopLat
+    val stopLon by StopTable.stopLon
+    val zoneId by StopTable.zoneId
+    val stopUrl by StopTable.stopUrl
+    val locationType by StopTable.locationType
+    val parentStation by StopTable.parentStation
+    val stopTimezone by StopTable.stopTimezone
+    val wheelchairBoarding by StopTable.wheelchairBoarding
+    //val levelId by StopTable.levelId
+    val platformCode by StopTable.platformCode
 }
 
