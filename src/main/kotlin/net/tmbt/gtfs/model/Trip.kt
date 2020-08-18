c29a4df2ac2d7eb32f40ffaca7365873d9fa15c4
Johannes Hengstler <ubezl@student.kit.edu>
2020-08-19 00:55:11 +0200

    simplified table layout

--- src/main/kotlin/net/tmbt/gtfs/model/Trip.kt
+++ src/main/kotlin/net/tmbt/gtfs/model/Trip.kt
@@ -1,5 +1,6 @@
 package net.tmbt.gtfs.model
 
+import net.tmbt.gtfs.util.ByOrdinal
 import org.jetbrains.exposed.dao.Entity
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IdTable
@@ -7,13 +8,15 @@ import org.jetbrains.exposed.sql.Column
 
 enum class TripDirection {
     OUTBOUND,
-    INBOUND
+    INBOUND;
+
+    companion object : ByOrdinal<TripDirection>(values())
 }
 
 object TripTable : IdTable<String>() {
-    val route = reference("route_id", RouteTable).entityId()
+    val route = reference("route_id", RouteTable)
+
     //val service = reference("service_id", CalendarDateTable) TODO: two tables?!
-    val trip = text("trip_id")
     val headsign = text("trip_headsign").nullable()
     val shortName = text("trip_short_name").nullable()
     val direction = enumeration("direction_id", TripDirection::class).nullable()
@@ -23,8 +26,7 @@ object TripTable : IdTable<String>() {
     val bikesAllowed = enumeration("bikes_allowed", Availability::class).nullable()
 
 
-    override val id: Column<EntityID<String>>
-        get() = TODO("Not yet implemented")
+    override val id: Column<EntityID<String>> = text("trip_id").entityId()
 
 }
 
@@ -32,7 +34,7 @@ class Trip(id: EntityID<String>) : Entity<String>(id) {
     var route by TripTable.route
 
     //var service  by TripTable.service
-    var trip by TripTable.trip
+    var trip by TripTable.id
     var headsign by TripTable.headsign
     var shortName by TripTable.shortName
     var direction by TripTable.direction
