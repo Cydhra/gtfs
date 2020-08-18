16b7ffb795d1622f973dfc739580545a165b60c8
Max Göttlicher <maxgoett@yahoo.de>
2020-08-19 01:35:29 +0200

    working on shape

--- src/main/kotlin/net/tmbt/gtfs/model/Shapes.kt
+++ src/main/kotlin/net/tmbt/gtfs/model/Shapes.kt
@@ -1,11 +1,22 @@
 package net.tmbt.gtfs.model
 
+import net.tmbt.gtfs.model.StopTable.nullable
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IdTable
 import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.select
 
 object ShapeTable : IdTable<String>() {
-    override val id: Column<EntityID<String>>
-        get() = TODO("Not yet implemented")
+    override val id: Column<EntityID<String>> = text("shape_id").entityId()
 
+    val lat = decimal("shape_pt_lat", 10, 7).nullable()
+    val lon = decimal("shape_pt_lon", 10, 7).nullable()
+    val sequence = integer("shape_pt_sequence")
+    val distTraveled = float("shape_dist_traveled").nullable()
+}
+
+class Shape(val id: String) {
+    init {
+        val result = ShapeTable.select { ShapeTable.id eq id}
+    }
 }
