0695f131d763f2cd0149b98c825a8f079fbfa9a3
Cydhra <ubezl@student.kit.edu>
2020-08-24 01:38:21 +0200

    implemented GtfsDatasetPath

--- src/test/kotlin/net/tmbt/gtfs/io/ConvenienceKtTest.kt
+++ src/test/kotlin/net/tmbt/gtfs/io/ConvenienceKtTest.kt
@@ -5,15 +5,23 @@ import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.exists
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.testng.Assert
+import org.testng.annotations.BeforeClass
 import org.testng.annotations.Test
 import java.nio.file.Files
 import java.nio.file.StandardCopyOption
 
+/**
+ * Unit tests for the convenience features in io.Convenience.kt
+ */
 internal class ConvenienceKtTest {
+
+    @BeforeClass
+    fun initDatabase() {
+        Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver", user = "root", password = "")
+    }
 
     @Test
     fun testDatabaseSetup() {
-        Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver", user = "root", password = "")
         transaction {
             updateDatabase()
 
@@ -43,7 +51,20 @@ internal class ConvenienceKtTest {
 
     @Test(dependsOnMethods = ["testDatabaseSetup"])
     fun testImportGtfsDatasetPath() {
+        val dataSetPath = createTempFile("kvv", ".zip")
+        dataSetPath.deleteOnExit()
 
+        javaClass.classLoader.getResourceAsStream("testdata.zip")!!.use {
+            Files.copy(it, dataSetPath.toPath(), StandardCopyOption.REPLACE_EXISTING)
+        }
+
+        transaction {
+            updateDatabase()
+
+            importGtfsDataset(dataSetPath.toPath())
+
+            // TODO test presence of dataset
+        }
     }
 
     @Test(dependsOnMethods = ["testDatabaseSetup"])
