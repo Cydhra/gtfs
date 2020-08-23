7a264cf5c339f0da5f6ec466cfea64578af2879e
Cydhra <ubezl@student.kit.edu>
2020-08-23 02:42:50 +0200

    typos and stylistic improvements

--- src/main/kotlin/net/tmbt/gtfs/model/StopTimes.kt
+++ src/main/kotlin/net/tmbt/gtfs/model/StopTimes.kt
@@ -1,18 +1,16 @@
 package net.tmbt.gtfs.model
 
-import net.tmbt.gtfs.model.StopTimeTable.nullable
+import net.tmbt.gtfs.util.ByOrdinal
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
-import org.jetbrains.exposed.dao.id.IdTable
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.Column
-import org.jetbrains.exposed.sql.Table
-import javax.swing.DropMode
 
 enum class TimeMode {
     APPROXIMATE,
-    EXACT
+    EXACT;
+
+    companion object : ByOrdinal<TimeMode>(values())
 }
 
 object StopTimeTable : IntIdTable() {
@@ -21,13 +19,13 @@ object StopTimeTable : IntIdTable() {
     val arrivalTime = text("arrival_time").nullable()
     val departureTime = text("departure_time").nullable()
     val stopSequence = integer("stop_sequence")
-    val headsign = text("stop_headsign").nullable()
+    val headSign = text("stop_headsign").nullable()
     val pickupType = enumeration("pickup_type", PickupMode::class).nullable()
     val dropOffType = enumeration("drop_off_type", PickupMode::class).nullable()
-    val continousPickup = enumeration("continous_pickup", PickupMode::class).nullable()
-    val continous = enumeration("continous_drop_off", PickupMode::class).nullable()
+    val continuousPickup = enumeration("continuous_pickup", PickupMode::class).nullable()
+    val continuous = enumeration("continuous_drop_off", PickupMode::class).nullable()
     val shapeDist = float("shape_dist_traveled").nullable()
-    val timepoint = enumeration("timepoint", TimeMode::class).nullable()
+    val timePoint = enumeration("timepoint", TimeMode::class).nullable()
 
     init {
         uniqueIndex(trip, stop)
@@ -42,12 +40,12 @@ class StopTime(id: EntityID<Int>) : IntEntity(id) {
     var arrivalTime by StopTimeTable.arrivalTime
     var departureTime by StopTimeTable.departureTime
     var stopSequence by StopTimeTable.stopSequence
-    var headsign by StopTimeTable.headsign
+    var headSign by StopTimeTable.headSign
     var pickupType by StopTimeTable.pickupType
     var dropOffType by StopTimeTable.dropOffType
-    var continousPickup by StopTimeTable.continousPickup
-    var continous by StopTimeTable.continous
+    var continuousPickup by StopTimeTable.continuousPickup
+    var continous by StopTimeTable.continuous
     var shapeDist by StopTimeTable.shapeDist
-    var timepoint by StopTimeTable.timepoint
+    var timePoint by StopTimeTable.timePoint
 }
 
