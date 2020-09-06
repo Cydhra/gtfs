236ab69a95f95e002d76824eb594894e69c3b3da
Johannes Hengstler <ubezl@student.kit.edu>
2020-09-06 23:04:31 +0200

    implemented Level entity and table

--- src/main/kotlin/net/tmbt/gtfs/model/Level.kt
+++ src/main/kotlin/net/tmbt/gtfs/model/Level.kt
@@ -1,11 +1,21 @@
 package net.tmbt.gtfs.model
 
+import org.jetbrains.exposed.dao.Entity
+import org.jetbrains.exposed.dao.EntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IdTable
 import org.jetbrains.exposed.sql.Column
 
 object LevelTable : IdTable<String>() {
-    override val id: Column<EntityID<String>>
-        get() = TODO("Not yet implemented")
+    override val id: Column<EntityID<String>> = varchar("level_id", MAX_IDENTIFIER_LENGTH).entityId()
+    val levelIndex = float("level_index")
+    val levelName = varchar("level_name", MAX_NAME_LENGTH).nullable()
+}
 
+class Level(id: EntityID<String>) : Entity<String>(id) {
+    companion object : EntityClass<String, Level>(LevelTable)
+
+    var levelId by LevelTable.id
+    var levelIndex by LevelTable.levelIndex
+    var levelName by LevelTable.levelName
 }
---
+++ src/main/kotlin/net/tmbt/gtfs/model/Model.kt
@@ -0,0 +1,4 @@
+package net.tmbt.gtfs.model
+
+const val MAX_IDENTIFIER_LENGTH = 255
+const val MAX_NAME_LENGTH = 4095
