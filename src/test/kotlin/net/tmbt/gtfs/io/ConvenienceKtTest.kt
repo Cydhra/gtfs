2bf66c5b4ef6e9bcc878fd9891e410097ce28ad9
Cydhra <ubezl@student.kit.edu>
2020-09-08 18:40:27 +0200

    test integrity of imported test data

--- src/test/kotlin/net/tmbt/gtfs/io/ConvenienceKtTest.kt
+++ src/test/kotlin/net/tmbt/gtfs/io/ConvenienceKtTest.kt
@@ -1,15 +1,17 @@
 package net.tmbt.gtfs.io
 
 import net.tmbt.gtfs.model.*
+import org.jetbrains.exposed.dao.Entity
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.exists
 import org.jetbrains.exposed.sql.transactions.transaction
-import org.testng.Assert
+import org.testng.Assert.*
 import org.testng.annotations.BeforeClass
 import org.testng.annotations.Test
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.StandardCopyOption
+import kotlin.reflect.KProperty
 
 /**
  * Unit tests for the convenience features in io.Convenience.kt
@@ -53,7 +55,7 @@ internal class ConvenienceKtTest {
                 TranslationTable,
                 AttributionTable
             ).forEach { table ->
-                Assert.assertTrue(table.exists())
+                assertTrue(table.exists())
             }
         }
     }
@@ -62,10 +64,8 @@ internal class ConvenienceKtTest {
     fun testImportGtfsDatasetPath() {
         transaction {
             updateDatabase()
-
             importGtfsDataset(dataSetPath.toPath())
-
-            // TODO test presence of dataset
+            testDataSet()
         }
     }
 
@@ -73,10 +73,65 @@ internal class ConvenienceKtTest {
     fun testImportGtfsDatasetUrl() {
         transaction {
             updateDatabase()
-
             importGtfsDataset(dataSetPath.toPath().toUri().toURL(), inMemory = true)
+            testDataSet()
+        }
+    }
 
-            // TODO test presence of dataset
+    /**
+     * A function to test the integrity of the imported data set. Does not test for the correctness of imported values,
+     * but merely for their presence. This means they still could be wrong (e.g. values interchanged within columns) but
+     * at least no columns are missing, that are present in the dataset
+     */
+    private fun testDataSet() {
+        val agencies = Agency.all()
+        assertEquals(1, agencies.count())
+        assertOptionalsPresent(agencies, Agency::lang, Agency::phone)
+
+        assertEquals(1, Calendar.all().count())
+        assertEquals(1, CalendarDate.all().count())
+
+        val routes = Route.all()
+        assertEquals(1, routes.count())
+        assertOptionalsPresent(routes, Route::agency, Route::shortName, Route::color, Route::textColor)
+
+        val stopTimes = StopTime.all()
+        assertEquals(1, stopTimes.count())
+        assertOptionalsPresent(
+            stopTimes,
+            StopTime::arrivalTime,
+            StopTime::departureTime,
+            StopTime::pickupType,
+            StopTime::dropOffType
+        )
+
+        val stops = Stop.all()
+        assertEquals(4, stops.count())
+        assertOptionalsPresent(
+            stops,
+            Stop::name,
+            Stop::lat,
+            Stop::lon
+        )
+
+        val trips = Trip.all()
+        assertEquals(1, trips.count())
+        assertOptionalsPresent(
+            trips,
+            Trip::headsign,
+            Trip::direction,
+            Trip::bikesAllowed
+        )
+    }
+
+    private fun assertOptionalsPresent(entities: Iterable<Entity<*>>, vararg properties: KProperty<*>) {
+        entities.forEach { entity ->
+            properties.forEach { property ->
+                assertNotNull(
+                    property.getter.call(entity),
+                    "${property.name} is not present in ${entity.javaClass.simpleName} \"${entity.id}\""
+                )
+            }
         }
     }
 }
