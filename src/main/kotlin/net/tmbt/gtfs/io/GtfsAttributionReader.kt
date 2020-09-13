9b8538164d6740c82539fb4e76e889f9455ae472
Cydhra <ubezl@student.kit.edu>
2020-09-13 17:22:59 +0200

    fixed attributions reader

--- src/main/kotlin/net/tmbt/gtfs/io/GtfsAttributionReader.kt
+++ src/main/kotlin/net/tmbt/gtfs/io/GtfsAttributionReader.kt
@@ -5,36 +5,27 @@ import net.tmbt.gtfs.model.AttributionTable
 import net.tmbt.gtfs.model.RouteTable
 import net.tmbt.gtfs.model.TripTable
 import org.jetbrains.exposed.dao.id.EntityID
-import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.insertAndGetId
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.io.InputStream
 
-class GtfsAttributionReader(inputStream: InputStream) : GtfsReader<String>(inputStream) {
-
-    override fun insertEntity(entries: Map<String, String>): EntityID<String> {
+class GtfsAttributionReader(inputStream: InputStream) : GtfsReader<Int>(inputStream) {
+    override fun insertEntity(entries: Map<String, String>): EntityID<Int> {
         return transaction {
-            val entityId = EntityID(
-                entries["attribution_id"] ?: error("cannot create attribution without id"),
-                AttributionTable
-            )
-
-            AttributionTable.insert { row ->
-                row[id] = entityId
-
+            AttributionTable.insertAndGetId { row ->
+                row[attributionId] = entries["attribution_id"]
                 row[agencyId] = entries["agency_id"]?.let { EntityID(it, AgencyTable) }
                 row[routeId] = entries["route_id"]?.let { EntityID(it, RouteTable) }
                 row[tripId] = entries["trip_id"]?.let { EntityID(it, TripTable) }
-                row[organizationName] =
-                    entries["organization_name"] ?: error("cannot create attribution without organization_name")
-                row[isProducer] = entries[""]?.let { it == "1" }
-                row[isOperator] = entries[""]?.let { it == "1" }
-                row[isAuthority] = entries[""]?.let { it == "1" }
+                row[organizationName] = entries["organization_name"]
+                    ?: error("cannot create an attribution without organization_name")
+                row[isProducer] = entries["is_producer"]?.let { it == "1" }
+                row[isOperator] = entries["is_operator"]?.let { it == "1" }
+                row[isAuthority] = entries["is_authority"]?.let { it == "1" }
                 row[attributionUrl] = entries["attribution_url"]
                 row[attributionEmail] = entries["attribution_email"]
                 row[attributionPhone] = entries["attribution_phone"]
             }
-
-            return@transaction entityId
         }
     }
 }
