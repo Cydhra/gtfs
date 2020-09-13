e94c660f9ed03274dbb6c91e2166b000a767fa71
Cydhra <ubezl@student.kit.edu>
2020-09-13 16:56:12 +0200

    fixed attributions id being not unique and added check constraint

--- src/main/kotlin/net/tmbt/gtfs/model/Attributions.kt
+++ src/main/kotlin/net/tmbt/gtfs/model/Attributions.kt
@@ -1,14 +1,18 @@
 package net.tmbt.gtfs.model
 
-import org.jetbrains.exposed.dao.Entity
-import org.jetbrains.exposed.dao.EntityClass
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
-import org.jetbrains.exposed.dao.id.IdTable
-import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.or
 
-object AttributionTable : IdTable<String>() {
-    override val id: Column<EntityID<String>> = varchar("attribution_id", MAX_IDENTIFIER_LENGTH).entityId()
-
+/**
+ * Table representing `attributions.txt`. However, as the GTFS does not specify a primary key an artificial integer key is
+ * generated for each entry.
+ */
+object AttributionTable : IntIdTable() {
+    val attributionId = varchar("attribution_id", MAX_IDENTIFIER_LENGTH).nullable()
     val agencyId = reference("agency_id", AgencyTable).nullable()
     val routeId = reference("route_id", RouteTable).nullable()
     val tripId = reference("trip_id", TripTable).nullable()
@@ -19,11 +23,21 @@ object AttributionTable : IdTable<String>() {
     val attributionUrl = varchar("attribution_url", MAX_TEXT_LENGTH).nullable()
     val attributionEmail = varchar("attribution_email", MAX_IDENTIFIER_LENGTH).nullable()
     val attributionPhone = varchar("attribution_phone", MAX_IDENTIFIER_LENGTH).nullable()
+
+    init {
+        check {
+            (agencyId.isNull() and routeId.isNull() and tripId.isNull()) or
+                    (agencyId.isNotNull() and routeId.isNull() and tripId.isNull()) or
+                    (agencyId.isNull() and routeId.isNotNull() and tripId.isNull()) or
+                    (agencyId.isNull() and routeId.isNull() and tripId.isNotNull())
+        }
+    }
 }
 
-class Attribution(id: EntityID<String>) : Entity<String>(id) {
-    companion object : EntityClass<String, Attribution>(AttributionTable)
+class Attribution(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<Attribution>(AttributionTable)
 
+    var attributionId by AttributionTable.attributionId
     var agency by Agency optionalReferencedOn AttributionTable.agencyId
     var route by Route optionalReferencedOn AttributionTable.routeId
     var trip by Trip optionalReferencedOn AttributionTable.tripId
